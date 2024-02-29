4cdf04984c2f61aa3139bca063e841ba2cd29320
Frank Zou <frankbearzou@gmail.com>
2024-02-28 20:39:56 -0500

    Create Solution.java

---
+++ 03 链表/01 图书整理 I/Solution.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public int[] reverseBookList(ListNode head) {
+        Stack<Integer> stack = new Stack<>();
+        while (head != null) {
+            stack.push(head.val);
+            head = head.next;
+        }
+        int[] ans = new int[stack.size()];
+        for (int i = 0; i < ans.length; i++) {
+            ans[i] = stack.pop();
+        }
+        return ans;
+    }
+}

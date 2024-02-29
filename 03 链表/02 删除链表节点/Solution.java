2208cdc3ee13a7b49875104b41f2cf6ae117826f
Frank Zou <frankbearzou@gmail.com>
2024-02-28 20:45:07 -0500

    Create Solution.java

---
+++ 03 链表/02 删除链表节点/Solution.java
@@ -0,0 +1,23 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public ListNode deleteNode(ListNode head, int val) {
+        ListNode dummy = new ListNode(0);
+        dummy.next = head;
+        ListNode cur = dummy;
+        while (cur.next != null) {
+            if (cur.next.val == val) {
+                cur.next = cur.next.next;
+                return dummy.next;
+            }
+            cur = cur.next;
+        }
+        return dummy.next;
+    }
+}

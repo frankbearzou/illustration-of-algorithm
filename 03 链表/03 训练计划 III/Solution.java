f4d7d7651c3df0dd0e9080860c763f303928013f
Frank Zou <frankbearzou@gmail.com>
2024-02-28 21:40:39 -0500

    Create Solution.java

---
+++ 03 链表/03 训练计划 III/Solution.java
@@ -0,0 +1,24 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode trainningPlan(ListNode head) {
+        if (head == null)
+            return head;
+        ListNode pre = null, cur = head;
+        while (cur != null) {
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+}

98f59146c7f673f607f8211741e84cdc88a3e858
Frank Zou <frankbearzou@gmail.com>
2024-03-04 20:08:51 -0500

    Update Solution.java

--- 04 数组/01 训练计划 I/Solution.java
+++ 04 数组/01 训练计划 I/Solution.java
@@ -1,22 +1,16 @@
 class Solution {
     public int[] trainingPlan(int[] actions) {
         int n = actions.length;
-        int[] ans = new int[n];
-        List<Integer> odd = new ArrayList<>();
-        List<Integer> even = new ArrayList<>();
-        for (int num : actions) {
-            if (num % 2 == 1)
-                odd.add(num);
-            else
-                even.add(num);
+        int l = 0, r = n - 1;
+        while (l < r) {
+            while (l < r && actions[l] % 2 == 1)
+                l++;
+            while (l < r && actions[r] % 2 == 0)
+                r--;
+            int t = actions[l];
+            actions[l] = actions[r];
+            actions[r] = t;
         }
-        int i = 0;
-        for (int num : odd) {
-            ans[i++] = num;
-        }
-        for (int num : even) {
-            ans[i++] = num;
-        }
-        return ans;
+        return actions;
     }
 }

0a14baebfadad49bbd22158a9a29e2bc7603ba39
Frank Zou <frankbearzou@gmail.com>
2024-03-07 08:31:01 -0500

    Create Solution.java

---
+++ 07 树/05 翻转二叉树/Solution.java
@@ -0,0 +1,21 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode(int x) { val = x; }
+ * }
+ */
+class Solution {
+    public TreeNode mirrorTree(TreeNode root) {
+        if (root == null)
+            return root;
+        TreeNode left = mirrorTree(root.left);
+        TreeNode right = mirrorTree(root.right);
+        TreeNode t = left;
+        root.left = right;
+        root.right = left;
+        return root;
+    }
+}

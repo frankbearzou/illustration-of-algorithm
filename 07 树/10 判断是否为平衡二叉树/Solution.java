d491eeded110dfa3029409da38e6ca39aeeff1f3
Frank Zou <frankbearzou@gmail.com>
2024-03-07 14:24:56 -0500

    Create Solution.java

---
+++ 07 树/10 判断是否为平衡二叉树/Solution.java
@@ -0,0 +1,32 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public boolean isBalanced(TreeNode root) {
+        return dfs(root) != -1;
+    }
+
+    int dfs(TreeNode root) {
+        if (root == null)
+            return 0;
+        int left = dfs(root.left);
+        int right = dfs(root.right);
+        if (left == -1 || right == -1)
+            return -1;
+        if (Math.abs(left - right) > 1)
+            return -1;
+        return Math.max(left, right) + 1;
+    }
+}

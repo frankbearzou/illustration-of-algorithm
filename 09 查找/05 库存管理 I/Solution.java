442c7cafc14e4b111fb0a8f099552a221d56e435
Frank Zou <frankbearzou@gmail.com>
2024-03-08 09:29:24 -0500

    Update Solution.java

--- 09 查找/05 库存管理 I/Solution.java
+++ 09 查找/05 库存管理 I/Solution.java
@@ -1,13 +1,14 @@
 class Solution {
     public int stockManagement(int[] stock) {
-        int n = stock.length;
-        int l = 0, r = n - 1;
+        int n = stock.length, l = 0, r = n - 1;
         while (l <= r) {
             int m = l + (r - l) / 2;
-            if (stock[m] <= stock[r])
-                r--;
-            else
+            if (stock[m] < stock[r])
+                r = m;
+            else if (stock[m] > stock[r])
                 l = m + 1;
+            else
+                r--;
         }
         return stock[l];
     }

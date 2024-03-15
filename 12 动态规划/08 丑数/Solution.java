98fdbfadf28c0638adbae93ab591f81f95366da1
Frank Zou <frankbearzou@gmail.com>
2024-03-15 09:18:11 -0400

    Create Solution.java

---
+++ 12 动态规划/08 丑数/Solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int nthUglyNumber(int n) {
+        int[] arr = new int[n];
+        arr[0] = 1;
+        int a = 0, b = 0, c = 0;
+        for (int i = 1; i < n; i++) {
+            arr[i] = Math.min(arr[a] * 2, Math.min(arr[b] * 3, arr[c] * 5));
+            if (arr[i] == arr[a] * 2)
+                a++;
+            if (arr[i] == arr[b] * 3)
+                b++;
+            if (arr[i] == arr[c] * 5)
+                c++;
+        }
+        return arr[n - 1];
+    }
+}
